3aaeca367a5c8142e884e4e4d7cece5e65033d76
Martin <joewwi03@gmail.com>
2023-03-28 13:58:46 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // your code here 
+  let form = document.querySelector("form")
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    manageTodo(e.target.new_task_description.value)
+    form.reset()
+  })
 });
+
+
+function manageTodo (todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+
+  li.textContent = `${todo} `
+  btn.textContent = "X"
+
+  li.appendChild(btn)
+
+  document.querySelector('#tasks').appendChild(li)  
+  btn.addEventListener('click', manageDelete)
+}
+
+function manageDelete(e){
+  e.target.parentNode.remove()
+}
+
